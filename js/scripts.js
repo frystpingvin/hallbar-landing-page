180130a5344cd6e8d8212d7431c407d149e41737
Samuel Lie <samuel.lie1@gmail.com>
2022-06-06 16:42:55 +0200

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - New Age v6.0.6 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+});
